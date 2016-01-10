5f860a70d03698d1d95fa54212d7d87d11b42734
HSH <scsc3313@naver.com>
2016-01-10 23:47:27 +0900

    while sentance is changed

--- projectEuler/Chatper2/src/com/company/Main.java
+++ projectEuler/Chatper2/src/com/company/Main.java
@@ -13,14 +13,16 @@ public class Main {
         int sum = 0;
 
         ArrayList<Integer> numbers = new ArrayList<>();
+        System.out.println("Input MAX fibonacciNumer");
         Scanner scanner = new Scanner(System.in);
         inputNum = scanner.nextInt();
 
         numbers.add(1);
         numbers.add(2);
+        System.out.println(1);
+        System.out.println(2);
 
-        while (fibonacciNum < inputNum){
-            fibonacciNum = numbers.get(index) + numbers.get(++index);
+        while ((fibonacciNum = numbers.get(index) + numbers.get(++index))  < inputNum){
             numbers.add(fibonacciNum);
             System.out.println(fibonacciNum);
         }
@@ -29,6 +31,7 @@ public class Main {
             if(numbers.get(i) % 2 == 0)
                 sum += numbers.get(i);
         }
+
         System.out.println("evenNumber's Total : " + sum);
 
 
