a57aeb42848de4b1c861214c02d6c820389994fa
HSH <scsc3313@naver.com>
2016-01-11 11:36:50 +0900

    Chapter3 is done

--- projectEuler/Chapter3/src/com/company/Main.java
+++ projectEuler/Chapter3/src/com/company/Main.java
@@ -6,22 +6,24 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-        int inputNum;
+        //600851475143 is inputNum;
+        long inputNum;
         ArrayList<Integer> primeFactorNum = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        inputNum = scanner.nextInt();
+        inputNum = scanner.nextLong();
 
-        for(int i = 2; i < inputNum ; i++){
+        for(int i = 2; i <= inputNum ; i++){
             if(inputNum % i == 0){
                 primeFactorNum.add(i);
                 inputNum /= i;
-                i = 2;
+                i = 1;
             }
         }
         StringBuilder primeFactorNums = new StringBuilder();
         primeFactorNums.append("primeFactors : ");
         for(int i = 0; i < primeFactorNum.size() ; i++){
+            if(primeFactorNum.size()-1 == (i))
+                primeFactorNums.append("MAX primeNumber is ");
             primeFactorNums
                     .append(primeFactorNum.get(i))
                     .append(" ");
