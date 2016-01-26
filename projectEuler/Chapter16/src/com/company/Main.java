b8b8f3201751f04fad9fa45f518fb4344895cad5
HSH <scsc3313@naver.com>
2016-01-26 22:48:09 +0900

    Chapter16 is done

--- projectEuler/Chapter16/src/com/company/Main.java
+++ projectEuler/Chapter16/src/com/company/Main.java
@@ -10,30 +10,32 @@ public class Main {
     }
 
     private void control(){
-        int result = 1;
-        int sum = 0;
+        int sum = 0; //각 자리 수를 더한 값
         ArrayList<Integer> numbers = new ArrayList<>();
-        int number = 10;
+        int number = 1000; //자리수! 1000으로 바꿀것.
+        numbers.add(2); //처음 시작하는 숫자
+        for(int j = 1; j < number ; j++){
 
-        for(int j = 0; j < number ; j++){
-            String resultString = String.valueOf(result);
-            char[] resultCharArray = resultString.toCharArray();
-
-            for(int i = resultCharArray.length - 1; i >= 0; i--){
-                int tempNumber = (resultCharArray[i] - 48)*2;
+            for(int i = numbers.size() - 1; i >= 0; i--){
+                int tempNumber = numbers.get(i)*2;
                 if(tempNumber > 9){
                     tempNumber -= 10;
-                    resultCharArray[i+1]++;
+                    if(numbers.size() -1 == i){
+                        numbers.add(1);
+                    }
+                    else {
+                        numbers.set(i+1, numbers.get(i+1)+1);
+                    }
                 }
-                numbers.add(tempNumber);
+                numbers.set(i, tempNumber);
             }
         }
 
 
         for(int i = 0 ; i < numbers.size(); i++){
-            System.out.println(numbers.get(i));
+            System.out.println("number.get : " + numbers.get(i));
             sum += numbers.get(i);
         }
-        System.out.println(sum);
+        System.out.println("sum : " + sum);
     }
 }
