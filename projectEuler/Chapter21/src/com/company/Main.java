7c0db78b2c8c75725227782ca171a8318b937662
HSH <scsc3313@naver.com>
2016-02-04 10:48:44 +0800

    thinking

--- projectEuler/Chapter21/src/com/company/Main.java
+++ projectEuler/Chapter21/src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Main main = new Main();
+        main.control();
+    }
+
+    private void control(){
+
     }
 }
