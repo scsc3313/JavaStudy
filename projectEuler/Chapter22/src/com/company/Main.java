934beeab61d3562ed3c02395e7be8a40b855ec2a
HSH <scsc3313@naver.com>
2016-02-06 21:56:42 +0900

    FileReader is completed

--- projectEuler/Chapter22/src/com/company/Main.java
+++ projectEuler/Chapter22/src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    private FileInputStream fileInputStream;
+    private BufferedInputStream bufferedInputStream;
+    private BufferedReader bufferedReader;
+
+    Main() throws FileNotFoundException {
+        fileInputStream = new FileInputStream(new File("names.txt"));
+        bufferedInputStream = new BufferedInputStream(fileInputStream);
+        bufferedReader = new BufferedReader(new FileReader("names.txt"));
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.control();
+    }
+
+    private void control() throws IOException {
+        String string = bufferedReader.readLine();
+        String[] names = string.split(",");
+        System.out.println(names[0]);
     }
 }
