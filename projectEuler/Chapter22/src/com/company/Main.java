e1ed4a337cc4ca115b0cca8413c5883036f8b88e
HSH <scsc3313@naver.com>
2016-02-06 22:16:08 +0900

    Chapter22 is done

--- projectEuler/Chapter22/src/com/company/Main.java
+++ projectEuler/Chapter22/src/com/company/Main.java
@@ -1,17 +1,16 @@
 package com.company;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
 
-    private FileInputStream fileInputStream;
-    private BufferedInputStream bufferedInputStream;
     private BufferedReader bufferedReader;
+    private int totalSum;
 
     Main() throws FileNotFoundException {
-        fileInputStream = new FileInputStream(new File("names.txt"));
-        bufferedInputStream = new BufferedInputStream(fileInputStream);
         bufferedReader = new BufferedReader(new FileReader("names.txt"));
+        totalSum = 0;
     }
 
     public static void main(String[] args) throws IOException {
@@ -22,6 +21,34 @@ public class Main {
     private void control() throws IOException {
         String string = bufferedReader.readLine();
         String[] names = string.split(",");
-        System.out.println(names[0]);
+
+        int nameLen = names.length;
+
+        int[] namesCount = new int[nameLen];
+        String[] onlyNames = new String[nameLen];
+
+        for(int i = 0; i < nameLen; i++){
+            onlyNames[i] = names[i].replace("\"", "");
+        }
+        Arrays.sort(onlyNames);
+        for(int i = 0; i < nameLen; i++){
+            namesCount[i] = calcuator(onlyNames[i]);
+        }
+        for(int i = 0; i < nameLen; i++){
+            totalSum += (i+1)*namesCount[i];
+        }
+        System.out.print("TotalSum is " + totalSum);
     }
+
+    private int calcuator(String name){
+        byte[] bytes = name.getBytes();
+        int sum = 0;
+        int[] ints = new int[bytes.length];
+        for(int i = 0; i < bytes.length ; i++){
+            ints[i] = bytes[i] - 64;
+            sum += ints[i];
+        }
+        return sum;
+    }
+
 }
