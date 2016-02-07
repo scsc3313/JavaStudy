da8b0302fc42710691a64875701d3968ce4eb300
HSH <scsc3313@naver.com>
2016-02-07 11:55:53 +0900

    Chapter 23 is done

---
+++ projectEuler/Chapter23/src/com/company/Main.java
@@ -0,0 +1,70 @@
+package com.company;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class Main {
+
+
+    private ArrayList<Integer> overNumbers;
+    private ArrayList<Integer> madeByOverNumbers;
+    private BigInteger totalSum;
+
+
+    Main(){
+        overNumbers = new ArrayList<>();
+        madeByOverNumbers = new ArrayList<>();
+        totalSum = BigInteger.valueOf(0);
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.control();
+    }
+
+    private void control(){
+        init();
+        makeIntegerByOverNumbers();
+
+        calculator();
+        System.out.print("TotalSum is " + totalSum);
+    }
+
+    private void calculator() {
+
+        for(int i = 0; i <= 28123; i++){
+            totalSum = totalSum.add(BigInteger.valueOf(i));
+        }
+        for(int i = 0; i < madeByOverNumbers.size() ; i++){
+            totalSum = totalSum.subtract(BigInteger.valueOf(i));
+        }
+
+    }
+
+    private void makeIntegerByOverNumbers() {
+        for(int i = 0; i < overNumbers.size() ; i++){
+            for(int j = 0; j < overNumbers.size() ; j++){
+                int tempSum = overNumbers.get(i) + overNumbers.get(j);
+                if(!madeByOverNumbers.contains(tempSum))
+                    madeByOverNumbers.add(tempSum);
+            }
+        }
+    }
+
+    private void init() {
+        for (int i = 0; i <= 28123; i++){
+            if(i < findPrimeNumberSum(i))
+                overNumbers.add(i);
+        }
+    }
+
+    private int findPrimeNumberSum(int inputNumber){
+        int sum = 0;
+        for(int i = 1; i < inputNumber ; i++){
+            if(inputNumber % i == 0)
+                sum += i;
+        }
+        return sum;
+    }
+
+}
