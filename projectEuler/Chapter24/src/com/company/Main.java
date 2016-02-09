c8ad628d1f143a0539852ac0d184d3155985c54d
HSH <scsc3313@naver.com>
2016-02-09 17:42:28 +0900

    Chapter24 is ongoing

--- projectEuler/Chapter24/src/com/company/Main.java
+++ projectEuler/Chapter24/src/com/company/Main.java
@@ -2,7 +2,11 @@ package com.company;
 
 public class Main {
 
+    private static final int[] staticNumbers = {0,1,2,3,4,5,6,7,8,9};
+
     public static void main(String[] args) {
-	// write your code here
+    }
+
+    private void initNumbers(){
     }
 }
