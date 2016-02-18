ed43dca3f6ff327286117ffa2b360964306c064a
HSH <scsc3313@naver.com>
2016-02-18 20:43:16 +0900

    Chapter30 is done

--- projectEuler/Chapter30/src/com/company/Main.java
+++ projectEuler/Chapter30/src/com/company/Main.java
@@ -10,18 +10,23 @@ public class Main {
     }
 
     private void control(){
-        String[] numbers = new String[90000];
+        String[] numbers = new String[354294];
         ArrayList<String> arrayList = new ArrayList<>();
-        for(int i = 0; i < 90000 ; i++){
-            numbers[i] = String.valueOf(i + 10000);
+        int totalSum = 0;
+        for(int i = 2; i < 354294 ; i++){
+            numbers[i] = String.valueOf(i);
             char[] dividedNumber = numbers[i].toCharArray();
             int sum = 0;
             for(int j = 0; j < dividedNumber.length; j++){
-                 sum += (dividedNumber[j]-48) * (dividedNumber[j]-48) * (dividedNumber[j]-48) * (dividedNumber[j]-48);
+                 sum += (dividedNumber[j]-48) * (dividedNumber[j]-48) * (dividedNumber[j]-48) * (dividedNumber[j]-48) * (dividedNumber[j]-48);
             }
             if(numbers[i].equals(String.valueOf(sum)))
                 arrayList.add(numbers[i]);
         }
-        System.out.println(arrayList.get(0));
+
+        for(int i = 0 ; i < arrayList.size(); i++){
+            totalSum += Integer.valueOf(arrayList.get(i));
+        }
+        System.out.println(totalSum);
     }
 }
