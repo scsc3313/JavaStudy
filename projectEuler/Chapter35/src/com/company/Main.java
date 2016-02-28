f1b6ff39d4d2da3f2740f45c4c60bd04273aed07
HSH <scsc3313@naver.com>
2016-02-28 15:28:33 +0900

    Chapter35 is ongoing

--- projectEuler/Chapter35/src/com/company/Main.java
+++ projectEuler/Chapter35/src/com/company/Main.java
@@ -11,9 +11,38 @@ public class Main {
 
     private void control(){
         ArrayList<Integer> arrayList = new ArrayList<>();
-        makePrimeNumbers(arrayList);
-        printPrimeNumbers(arrayList);
+        ArrayList<Integer> realArrayList = new ArrayList<>();
 
+        makePrimeNumbers(arrayList);
+        realArrayList = checkCirclePrimeNumber(arrayList);
+        printPrimeNumbers(realArrayList);
+    }
+
+    private ArrayList<Integer> checkCirclePrimeNumber(ArrayList<Integer> arrayList) {
+
+        ArrayList<Integer> realArrayList = new ArrayList<>();
+        for(int i = 0; i < arrayList.size(); i++){
+            int temp = 0;
+            if(arrayList.get(i) > 10){
+                while(temp != arrayList.get(i)){
+                    String tempString = String.valueOf(arrayList.get(i)%10) + String.valueOf(arrayList.get(i)/10);
+                    temp = Integer.valueOf(tempString);
+                    if(!checkPrimeNumber(temp, arrayList))
+                        break;
+                }
+                realArrayList.add(arrayList.get(i));
+
+            }
+        }
+        return realArrayList;
+    }
+
+    private boolean checkPrimeNumber(int temp, ArrayList<Integer> arrayList) {
+        for(int i = 0; i < arrayList.size(); i++){
+            if(temp == arrayList.get(i))
+                return true;
+        }
+        return false;
     }
 
     private void printPrimeNumbers(ArrayList<Integer> arrayList) {
@@ -23,7 +52,7 @@ public class Main {
     }
 
     private void makePrimeNumbers(ArrayList<Integer> arrayList) {
-        for(int i = 1; i <= 1000000; i++){
+        for(int i = 1; i <= 100; i++){
             int count = 0;
             for(int j = 1; j <= i ; j++){
                 if(i % j == 0)
