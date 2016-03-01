4da7ba8b36acb4342219ed98b3087a1bb0aac889
HSH <scsc3313@naver.com>
2016-03-01 13:02:19 +0900

    make primeNumbers

--- projectEuler/Chapter37/src/com/company/Main.java
+++ projectEuler/Chapter37/src/com/company/Main.java
@@ -1,8 +1,42 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
+    private int maxNumber;
+    private ArrayList<Integer> arrayList;
+
+
+    public Main() {
+        maxNumber = 1000;
+        arrayList = new ArrayList<>();
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        Main main = new Main();
+        main.control();
+    }
+
+    private void control(){
+        for(int i = 11; i < maxNumber ; i++){
+            int count = 0;
+            for(int j = 1 ; j <= i ; j++){
+                if( i % j == 0 )
+                    count++;
+                if(count > 2)
+                    break;
+            }
+            if(count == 2)
+                arrayList.add(i);
+        }
+
+        print(arrayList);
+    }
+
+    private void print(ArrayList<Integer> arrayList) {
+        for(int i = 0; i < arrayList.size(); i++){
+            System.out.println(arrayList.get(i));
+        }
     }
 }
