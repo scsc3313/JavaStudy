d2e4c0ff97dffd96033a8648c2bf02f4dc4f3455
HSH <scsc3313@naver.com>
2016-03-01 13:15:31 +0900

    Solve the problem using Array

--- projectEuler/Chapter37/src/com/company/Main.java
+++ projectEuler/Chapter37/src/com/company/Main.java
@@ -6,11 +6,13 @@ public class Main {
 
     private int maxNumber;
     private ArrayList<Integer> arrayList;
+    private ArrayList<Integer> realArrayList;
 
 
     public Main() {
         maxNumber = 1000;
         arrayList = new ArrayList<>();
+        realArrayList = new ArrayList<>();
     }
 
     public static void main(String[] args) {
@@ -19,6 +21,40 @@ public class Main {
     }
 
     private void control(){
+        makePrimeNumbers();
+        print(arrayList);
+        findRealPrimeNumbers(arrayList);
+    }
+
+    private void findRealPrimeNumbers(ArrayList<Integer> arrayList) {
+        for(int i = 0; i < arrayList.size(); i++){
+            int tempNumber = arrayList.get(i);
+            while(tempNumber >= 10){
+                tempNumber /= 10;
+                if(checkIsPrimeNumber(tempNumber))
+                    break;
+            }
+            String stringNumber = String.valueOf(tempNumber);
+            char[] arrayNumber = stringNumber.toCharArray();
+            while(arrayNumber.length != 1){
+                stringNumber = "";
+                for(int j = 0; j < arrayNumber.length; j++){
+                    stringNumber += arrayNumber[j];
+                }
+            }
+        }
+    }
+
+    private boolean checkIsPrimeNumber(int tempNumber) {
+        for(int i = 0; i < arrayList.size(); i++){
+            if(tempNumber == arrayList.get(i))
+                return true;
+        }
+        return false;
+    }
+
+
+    private void makePrimeNumbers() {
         for(int i = 11; i < maxNumber ; i++){
             int count = 0;
             for(int j = 1 ; j <= i ; j++){
@@ -30,8 +66,6 @@ public class Main {
             if(count == 2)
                 arrayList.add(i);
         }
-
-        print(arrayList);
     }
 
     private void print(ArrayList<Integer> arrayList) {
