bc7b624ae9400e6bf0e8020166d5e716f64b38cc
HSH <scsc3313@naver.com>
2016-03-11 22:55:04 +0900

    Start Chapter39

---
+++ projectEuler/Chapter39/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.control();
+    }
+
+    private void control() {
+
+    }
+}
