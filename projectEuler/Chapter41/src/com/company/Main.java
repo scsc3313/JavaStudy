ce30ae26f5b8abe0c94d2f53902f0f772c4914b0
HSH <scsc3313@naver.com>
2016-03-14 15:47:11 +0900

    Start

---
+++ projectEuler/Chapter41/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    private ArrayList<Integer> arrayList;
+
+    Main(){
+        arrayList = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.control();
+    }
+
+    private void control() {
+        makePandigitdalNumbers();
+
+    }
+
+    private void makePandigitdalNumbers() {
+
+    }
+}
