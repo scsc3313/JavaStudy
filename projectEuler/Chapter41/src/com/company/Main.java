5406d5bd384e1cb35aa958a99bd10dd6147b3e48
HSH <scsc3313@naver.com>
2016-03-20 22:27:25 +0900

    Chapter41 is done

--- projectEuler/Chapter41/src/com/company/Main.java
+++ projectEuler/Chapter41/src/com/company/Main.java
@@ -1,14 +1,6 @@
 package com.company;
 
-import java.util.ArrayList;
-
 public class Main {
-
-    private ArrayList<Integer> arrayList;
-
-    Main(){
-        arrayList = new ArrayList<>();
-    }
 
     public static void main(String[] args) {
         Main main = new Main();
@@ -16,11 +8,41 @@ public class Main {
     }
 
     private void control() {
-        makePandigitdalNumbers();
-
+        System.out.println("The biggest PandigitalNumber" + findPandigitalNumber());
     }
 
-    private void makePandigitdalNumbers() {
-
+    //Find FirstBig Pandigital and Prime Number
+    private int findPandigitalNumber() {
+        for(int i = 987654321; i > 0; i--){
+            if(isPandigital(i)){
+                return i;
+            }
+        }
+        return 0;
+    }
+    //Check PrimeNum && PandigitalNum
+    private boolean isPandigital(int num) {
+        int count = 0;
+        for(int i = 1 ; i < num; i ++){
+            if(num % i == 0)
+                count++;
+            if(count > 2)
+                return false;
+        }
+        String numStr = String.valueOf(num);
+        char[] nums = numStr.toCharArray();
+        for(int i = 1; i <= nums.length; i++){
+            for(int j = 0 ; j < nums.length ; j++){
+                if((nums[j] - 48) == i){
+                    nums[j] = 64;
+                    break;
+                }
+            }
+        }
+        for(int i = 0; i < nums.length ; i++){
+            if(nums[i] != 64)
+                return false;
+        }
+        return true;
     }
 }
